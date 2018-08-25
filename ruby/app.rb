343fa008fc03e359902cc38570fe88df24fb1c7e
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2018-08-25 17:06:08 +0900

    Add code about db insert

--- ruby/app.rb
+++ ruby/app.rb
@@ -313,9 +313,13 @@ class App < Sinatra::Base
     end
 
     if !avatar_name.nil? && !avatar_data.nil?
-      statement = db.prepare('INSERT INTO image (name, data) VALUES (?, ?)')
-      statement.execute(avatar_name, avatar_data)
-      statement.close
+      # TODO: ここにdbへ追加する処理を行う。
+      File.open("../public/image/#{avatar_name}", "w+") do |f|
+        f.puts(avatar_data)
+      end
+      # statement = db.prepare('INSERT INTO image (name, data) VALUES (?, ?)')
+      # statement.execute(avatar_name, avatar_data)
+      # statement.close
       statement = db.prepare('UPDATE user SET avatar_icon = ? WHERE id = ?')
       statement.execute(avatar_name, user['id'])
       statement.close
